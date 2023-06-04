9e0571f203183115a1f2ddadae16750224039911
udhungel <udeepdhungel@gmail.com>
2023-06-04 22:33:05 +0200

    Added CouponApiController and Deleted default WeatherForeCase Controller

---
+++ CouponWebAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,53 @@
+﻿using CouponWebAPI.Data;
+using CouponWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CouponWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList(); 
+                return objList; 
+
+            }
+            catch (Exception)
+            {
+
+               
+            }
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon objList = _db.Coupons.First(u=>u.CouponId == id);
+                return objList;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+         
+        }
+    }
+}
