b2c0a424e974a68bcc3486f61a01bef3521605fc
udhungel <udeepdhungel@gmail.com>
2023-08-02 23:38:14 +0200

    Section 3 : Added all DTO's for authentication of User

---
+++ Mango.Services.AuthAPI/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using CouponWebAPI.Models.Dto;
+
+namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class LoginResponseDto
+    {
+        public UserDto User { get; set; }
+        public string Token { get; set; }       
+    }
+}
---
+++ Mango.Services.AuthAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CouponWebAPI.Models.Dto
+{
+    public class ResponseDto
+    {
+        public Object? Result { get; set; }
+
+        public bool IsSuccess { get; set; } = true;
+
+        public string Message { get; set; } = "";
+    }
+}
