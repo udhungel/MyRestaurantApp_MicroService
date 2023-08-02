72132d02c0766fa6b70b05d875f68efe263e71c9
udhungel <udeepdhungel@gmail.com>
2023-08-02 22:02:09 +0200

    Section 2: Create logic Controller Action

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -27,9 +27,22 @@ namespace Mango.Web.Controllers
 
         public async Task<IActionResult> CouponCreate() 
         {
-            
-
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {               
+                ResponseDto? response = await _couponService.CreateCouponAsync(model);
+                if (response != null && response.IsSuccess)
+                {
+                   return RedirectToAction(nameof(CouponIndex));
+                }
+
+            }
+            return View(model);
         }
     }
 
