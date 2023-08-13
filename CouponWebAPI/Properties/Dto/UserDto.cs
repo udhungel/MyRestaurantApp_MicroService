f1bdef26983c007aa5757c632a6333589669981e
udhungel <udeepdhungel@gmail.com>
2023-08-13 21:13:54 +0200

    Section 3: Add Dto for log in inside Auth Api

---
+++ CouponWebAPI/Properties/Dto/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CouponWebAPI.Models.Dto
+{
+    public class UserDto
+    {        
+        public string ID { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+    }
+}
